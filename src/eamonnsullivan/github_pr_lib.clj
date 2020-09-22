526b7ddfcc274eeba6983dbf668de380af7e1c54
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2020-09-22 18:12:18 +0100

    Get repo id, mutation

--- src/eamonnsullivan/github_pr_lib.clj
+++ src/eamonnsullivan/github_pr_lib.clj
@@ -1,9 +1,31 @@
 (ns eamonnsullivan.github-pr-lib
   (:require [clj-http.client :as client]
-            [clojure-string :as string]
+            [clojure.string :as string]
             [clojure.data.json :as json]))
 
 (def github-url "https://api.github.com/graphql")
+
+(def get-repo-id-query "query($owner: String!, $name: String!) {
+    repository(owner:$owner, name:$name) {
+      id
+    }
+  }")
+
+(def create-pull-request-query "mutation
+($title: String!, $body: String!, $repositoryId: ID!, $baseBranch: String!, $mergingBranch: String!, $draft: Boolean!) {
+  createPullRequest(input: {
+    title: $title,
+    body: $body,
+    repositoryId: $repositoryId,
+    baseRefName: $baseBranch,
+    headRefName: $mergingBranch,
+    draft: $draft
+  }) {
+    pullRequest {
+      permalink
+    }
+  }
+}")
 
 (defn request-opts
   [access-token]
@@ -13,7 +35,9 @@
   [url payload opts]
   (client/post url (merge {:content-type :json :body payload} opts)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn get-repo-id
+  [access-token owner repo-name]
+  (let [variables {:owner owner :name repo-name}
+        payload (json/write-str {:query get-repo-id-query :variables variables})
+        response (http-post github-url payload (request-opts access-token))]
+    (-> (json/read-str (response :body) :key-fn keyword) :data :repository :id)))
--- test/eamonnsullivan/github_pr_lib_test.clj
+++ test/eamonnsullivan/github_pr_lib_test.clj
@@ -1,7 +1,15 @@
 (ns eamonnsullivan.github-pr-lib-test
   (:require [clojure.test :refer :all]
-            [eamonnsullivan.github-pr-lib :refer :all]))
+            [eamonnsullivan.github-pr-lib :as sut]
+            [clojure.data.json :as json]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def repo-id-response-success (slurp "./test/eamonnsullivan/repo-response-success.json"))
+(def repo-id-response-failure (slurp "./test/eamonnsullivan/repo-response-failure.json"))
+
+(deftest test-get-repo-id
+  (with-redefs [sut/http-post (fn [_ _ _] {:body repo-id-response-success})]
+    (testing "parses id from successful response"
+      (is (= "MDEwOlJlcG9zaXRvcnkyOTczNzY1NTc=" (sut/get-repo-id "secret-token" "owner" "repo-name")))))
+  (with-redefs [sut/http-post (fn [_ _ _] {:body repo-id-response-failure})]
+    (testing "returns nil on error"
+      (is (= nil (sut/get-repo-id "secret-token" "owner" "repo-name"))))))
